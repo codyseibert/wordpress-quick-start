32ff8b065710d12cd1feaea4bc91e768f009f7c6
Cody Seibert <cseibert113@gmail.com>
2021-06-21 20:17:26 -0400

    adding a -y to skip the prompt

--- setup.sh
+++ setup.sh
@@ -16,8 +16,8 @@ echo \
 docker --version
 
 # installing docker-compose
-apt-get update
-apt-get install docker-ce docker-ce-cli containerd.io
+apt-get update -y
+apt-get install -y docker-ce docker-ce-cli containerd.io
 curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 docker-compose --version
